eb8c0c2afa685727641bc653cc95f78c252d9233
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2012-12-15 13:19:06 +0100

    - Applying ruby styleguide to helpers

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,23 +1,22 @@
 module ConversationsHelper
   def message_classes(alternate, message, current_user)
-    [("alternate" if alternate), ("unread" if message.sender != current_user && message.unread?)].join(" ")
+    [('alternate' if alternate), ('unread' if message.sender != current_user && message.unread?)].join(' ')
   end
 
   def message_timestamp(message)
-    content_tag(:small, title: I18n.l(message.created_at.in_time_zone(current_user.time_zone), format: :long), class: "pull-right muted") do
-      I18n.t("conversations.messages.time_ago", time: distance_of_time_in_words(message.created_at, Time.now.utc, true))
+    content_tag(:small, title: I18n.l(message.created_at.in_time_zone(current_user.time_zone), format: :long), class: 'pull-right muted') do
+      I18n.t('conversations.messages.time_ago', time: distance_of_time_in_words(message.created_at, Time.now.utc, true))
     end
   end
 
   def message_readat(message)
-    content_tag(:small, class: "muted") do
-      content_tag(:i, nil, class: "icon-ok") + " " +
-      I18n.t("conversations.messages.seen", date: l(message.read.in_time_zone(current_user.time_zone), format: :short))
+    content_tag(:small, class: 'muted') do
+      content_tag(:i, nil, class: 'icon-ok') + ' ' +
+      I18n.t('conversations.messages.seen', date: l(message.read.in_time_zone(current_user.time_zone), format: :short))
     end
   end
 
   def new_or_show_conversation_path(conversation)
     conversation && conversation.persisted? ? conversation_path(conversation) : new_conversation_path(itinerary_id: conversation.conversable_id)
   end
-
 end
