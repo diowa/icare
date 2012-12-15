a6b3f1b26351649037b7d195bfccd9bfb9a8fe8d
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2012-12-15 22:21:47 +0100

    applying style guide to user class and workers

--- app/workers/facebook_data_cacher.rb
+++ app/workers/facebook_data_cacher.rb
@@ -4,7 +4,7 @@ class FacebookDataCacher
   def self.perform(user_id)
     user = User.find user_id
     if user.facebook_data_updated_at.nil? || user.facebook_data_updated_at < APP_CONFIG.facebook.cache_expiry_time.ago.utc
-      if user.cache_facebook_data
+      if user.cache_facebook_data?
         user.update_attribute :facebook_data_updated_at, Time.now.utc
       else
         # TODO add to task rescheduling list
