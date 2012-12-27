01e94072448d3e1078ba31e28245042f2719c054
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2012-12-27 18:53:32 +0100

    improved Facebook timeline publisher

--- app/workers/facebook_timeline_publisher.rb
+++ app/workers/facebook_timeline_publisher.rb
@@ -3,13 +3,11 @@ class FacebookTimelinePublisher
 
   def self.perform(itinerary_id)
     itinerary = Itinerary.find itinerary_id
-    itinerary_url = Rails.application.routes.url_helpers.itinerary_url(itinerary, host: APP_CONFIG.base_url)
+    itinerary_url = Rails.application.routes.url_helpers.itinerary_url itinerary, host: APP_CONFIG.base_url
     user = itinerary.user
+    return unless user.has_facebook_permission?(:publish_stream)
     user.facebook do |fb|
-      permissions = fb.get_connections 'me', 'permissions'
-      if permissions[0]['publish_stream'].to_i == 1
-        fb.put_connections('me', "#{APP_CONFIG.facebook.namespace}:plan", itinerary: itinerary_url)
-      end
+      fb.put_connections 'me', "#{APP_CONFIG.facebook.namespace}:plan", itinerary: itinerary_url
     end
   end
 end
