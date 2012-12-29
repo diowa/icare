96bbfa579482a3c0ccd8368c5efb68db7bcf2f2f
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2012-12-29 17:28:08 +0100

    improved users helper removed unused methods in references helper

--- app/helpers/references_helper.rb
+++ app/helpers/references_helper.rb
@@ -16,9 +16,9 @@ module ReferencesHelper
 
   def make_thumbs(rating)
     if rating == -1
-      content_tag(:i, nil, class: "icon-thumbs-down")
+      content_tag :i, nil, class: 'icon-thumbs-down'
     elsif rating == 1
-      content_tag(:i, nil, class: "icon-thumbs-up")
+      content_tag :i, nil, class: 'icon-thumbs-up'
     end
   end
 
@@ -34,25 +34,6 @@ module ReferencesHelper
         t(".reference_travelling_help")
       end
     end
-  end
-
-  def reference_snippet(reference)
-    [ make_thumbs(reference.rating),
-    " ",
-    content_tag(:b) do
-      link_to reference.referencing_user, user_path(reference.referencing_user)
-    end,
-    ": ",
-    content_tag(:em) do
-     "&laquo;#{reference.message}&raquo;".html_safe
-   end,
-   " ",
-   if reference.is_host
-     content_tag(:i, nil, class: "icon-home")
-   else
-     content_tag(:i, nil, class: "icon-plane")
-   end
-   ].join.html_safe
   end
 
   def new_or_show_reference_path(reference, itinerary)
