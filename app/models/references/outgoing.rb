90f11396d903538aa1e33cc8f0fd3d5546e9f093
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2012-12-30 14:07:08 +0100

    added todo in reference outgoing

--- app/models/references/outgoing.rb
+++ app/models/references/outgoing.rb
@@ -6,11 +6,12 @@ class References::Outgoing < References::Base
     # Notifications and Incoming Reference
     if changed?
       other_user = User.find reference.referencing_user_id
+      # TODO improve this method with strong parameters
       other_reference = other_user.references.where(itinerary: reference.itinerary,
                                                     referencing_user_id: reference.user.id).first || other_user.references.build
       other_reference.itinerary = reference.itinerary
       other_reference.referencing_user_id = reference.user.id
-      other_reference.build_incoming(rating: rating, body: body)
+      other_reference.build_incoming rating: rating, body: body
       other_reference.save
     end
   end
