9fa2bf1faee029e503a3af7993c0632fceb3885c
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2012-12-31 19:23:32 +0100

    improved itinerary search service object, added another spec for reversed itinerary

--- app/services/itinerary_search.rb
+++ app/services/itinerary_search.rb
@@ -1,4 +1,6 @@
 class ItinerarySearch
+  SPHERE_RADIUS = 5.fdiv Mongoid::Geospatial.earth_radius[:km]
+
   def initialize(params, user)
     @params = params
     @user = user
@@ -6,36 +8,33 @@ class ItinerarySearch
 
   def itineraries
     # TODO: Optimization
-    start_location = [@params[:start_location_lng].to_f, @params[:start_location_lat].to_f]
-    end_location = [@params[:end_location_lng].to_f, @params[:end_location_lat].to_f]
-    sphere_radius = 5.fdiv Mongoid::Geospatial.earth_radius[:km]
+    a_location = [@params[:start_location_lng].to_f, @params[:start_location_lat].to_f]
+    b_location = [@params[:end_location_lng].to_f, @params[:end_location_lat].to_f]
 
-    # Apply filters
+    # Applying filters
     filters = extract_filters_from_params
     itineraries = Itinerary.includes(:user).where(filters)
 
-    # Overrides pink filter for malicious male users
-    itineraries = itineraries.where(pink: false) if @user.male?
+    # Getting itineraries from A to B
+    from_a_to_b_itineraries = itineraries.within_spherical_circle(start_location: [ a_location, SPHERE_RADIUS ]) \
+                              & itineraries.within_spherical_circle(end_location: [ b_location, SPHERE_RADIUS ])
 
-    # From start to end
-    normal_itineraries = itineraries.within_spherical_circle(start_location: [ start_location, sphere_radius ]) &
-                         itineraries.within_spherical_circle(end_location: [ end_location, sphere_radius ])
-
-    # From end to start, unless passenger searched for a round trip
-    # NOTE: Think about it, because driver may need a travelmate for the whole trip
-    reversed_itineraries = []
-    unless filters[:round_trip]
-      reversed_itineraries = itineraries.where(round_trip: true).within_spherical_circle(start_location: [ end_location, sphere_radius ]) &
-                             itineraries.within_spherical_circle(end_location: [ start_location, sphere_radius ])
-    end
+    # From B to A, unless passenger searched for a round trip
+    # NOTE: Think about it - driver may need a travelmate for the whole trip
+    from_b_to_a_itineraries = filters[:round_trip] ? [] : get_reversed(itineraries, a_location, b_location)
 
     # Sum results
-    normal_itineraries + reversed_itineraries
+    from_a_to_b_itineraries + from_b_to_a_itineraries
   rescue
     []
   end
 
   private
+  def get_reversed(itineraries, a_location, b_location)
+    itineraries.where(round_trip: true).within_spherical_circle(start_location: [ b_location, SPHERE_RADIUS ]) \
+    & itineraries.within_spherical_circle(end_location: [ a_location, SPHERE_RADIUS ])
+  end
+
   def extract_filters_from_params
     filters = {}
 
@@ -43,6 +42,9 @@ class ItinerarySearch
       param = @params["filter_#{checkbox_field}".to_sym]
       filters.merge!(checkbox_field => true) if param == '1'
     end
+
+    # Overrides pink filter for malicious male users
+    filters.merge!(pink: false) if @user.male?
 
     [:smoking_allowed, :pets_allowed].each do |boolean_field|
       param = @params["filter_#{boolean_field}".to_sym]
