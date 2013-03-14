d29bb983c012f99229034c33359183ee70de084f
Geremia Taglialatela <tagliala.dev@gmail.com>
2013-03-14 22:16:15 +0100

    Fixes bullet issue Improves locale detection Fixes #11

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -4,5 +4,4 @@ if defined? Bullet
   Bullet.bullet_logger = true
   Bullet.console = true
   Bullet.rails_logger = true
-  Bullet.disable_browser_cache = true
 end
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -15,4 +15,34 @@ describe 'Application' do
       expect(current_path).to eq banned_path
     end
   end
+
+  describe 'Locale' do
+    it "fallbacks to en-US when user is passing an unknown locale param" do
+      user = FactoryGirl.create :user, uid: '123456'
+      visit '/auth/facebook'
+      visit itineraries_user_path(user, locale: 'XX-ZZ')
+      expect(page).to have_content I18n.t('users.itineraries.title', locale: 'en-US')
+    end
+
+    it "fallbacks to en-US when user is passing a compatible locale param" do
+      user = FactoryGirl.create :user, uid: '123456'
+      visit '/auth/facebook'
+      visit itineraries_user_path(user, locale: 'en-XX')
+      expect(page).to have_content I18n.t('users.itineraries.title', locale: 'en-US')
+    end
+
+    it "fallbacks to en-US when user is coming from facebook with a compatible locale" do
+      user = FactoryGirl.create :user, uid: '123456', locale: 'en-YY'
+      visit '/auth/facebook'
+      visit itineraries_user_path(user)
+      expect(page).to have_content I18n.t('users.itineraries.title', locale: 'en-US')
+    end
+
+    it "fallbacks to en-US when user is using en locale" do
+      user = FactoryGirl.create :user, uid: '123456', locale: 'en-GB'
+      visit '/auth/facebook'
+      visit itineraries_user_path(user, locale: 'en')
+      expect(page).to have_content I18n.t('users.itineraries.title', locale: 'en-US')
+    end
+  end
 end
