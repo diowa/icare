16031fb2b73183b2c11c44ef76da37a5169f96eb
hexlator <hexlator@gmail.com>
2014-09-22 17:25:02 -0700

    modify application.rb and local en.yml

--- config/settings/application.rb
+++ config/settings/application.rb
@@ -5,21 +5,21 @@
 # SET SENSITIVE DATA ONLY IN 'local.rb'
 
 SimpleConfig.for :application do
-  set :app_name, 'icare'
-  set :repository_url, 'http://github.com/diowa/icare'
+  set :app_name, 'Blacoin'
+  set :repository_url, 'http://www.blacoin.com'
 
   set :available_locales, Hash[{ :"en-US" => 'English (US)',
                                  :"it-IT" => 'Italiano' }.sort_by { |_, native_name| native_name }]
 
-  set :demo_mode, true
-  set :base_url, '127.0.0.1:5000'
+  set :demo_mode, false
+  set :base_url, 'http://www.blacoin.com'
   set :secret_token, '197241fc4c041de6402aa732e0004c5401536237a1c39178005ddf9994695cfc71fb32b543f8fb216f272b416974e3ea3cece241278a40a8516291aec598a948'
   set :single_process_mode, true
 
   set :costs_calculation_service_link, 'http://servizi.aci.it/CKInternet/'
 
-  set :google_analytics_id, nil
-  set :google_maps_api_key, nil
+  set :google_analytics_id, 41736893
+  set :google_maps_api_key, AIzaSyAOqKhzIt6GYQS7eplWxl1VadWMzJspwTo
 
   group :devise do
     set :secret_key, 'eb72ea40d147d8cc0c87dda38b498f7650ccd9286d38dc220fb16d69d4a0623b12b6ca294e098420df506352470addf14aaf2dcff980aa49f1529578de500dda'
@@ -29,7 +29,8 @@ SimpleConfig.for :application do
     set :namespace, 'FACEBOOK_NAMESPACE'
     set :app_id, 'FACEBOOK_APP_ID'
     set :secret, 'FACEBOOK_SECRET'
-    set :scope, 'email, publish_stream, user_birthday, user_about_me, user_education_history, user_interests, user_likes, user_religion_politics, user_work_history'
+    #set :scope, 'email, publish_stream, user_birthday, user_about_me, user_education_history, user_interests, user_likes, user_religion_politics, user_work_history'
+    set : scope, 'publish_stream'
     set :cache_expiry_time, 7.days
     # If set, only users of the following facebook group id will be able to use the application
     # It will automatically add 'user_groups' permission to facebook scope
@@ -64,7 +65,7 @@ SimpleConfig.for :application do
   end
 
   group :mailer do
-    set :from, "\"Icare\" <no-reply@i.care>"
+    set :from, "\"blacoin\" <no-reply@blacoin.com>"
     set :host, '127.0.0.1'
 
     group :smtp_settings do
