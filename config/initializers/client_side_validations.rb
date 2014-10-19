4363c020e2c06ed361a6f9ac81b52b5b595a767a
Geremia Taglialatela <tagliala.dev@gmail.com>
2014-10-19 12:46:55 +0200

    Updated csv initializer

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -3,7 +3,14 @@
 # Uncomment to disable uniqueness validator, possible security issue
 ClientSideValidations::Config.disabled_validators = [:uniqueness]
 
+# Uncomment to validate number format with current I18n locale
+# ClientSideValidations::Config.number_format_with_locale = true
+
 # Uncomment the following block if you want each input field to have the validation messages attached.
+#
+# Note: client_side_validation requires the error to be encapsulated within
+# <label for="#{instance.send(:tag_id)}" class="message"></label>
+#
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
 #   unless html_tag =~ /^<label/
 #     %{<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" class="message">#{instance.error_message.first}</label></div>}.html_safe
@@ -12,3 +19,4 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
 #   end
   html_tag.html_safe
 end
+
