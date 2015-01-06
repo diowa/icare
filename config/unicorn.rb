5e90a73ad7e2e3a760a9eb5d430232bc22586f02
Geremia Taglialatela <tagliala.dev@gmail.com>
2015-01-06 16:20:16 +0100

    another attempt

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-# Code borrowed from Diaspora
+# From Diaspora
 
 worker_processes 3
 timeout 30
@@ -6,18 +6,22 @@ preload_app true
 
 @resque_pid = nil
 
-unless ::APP_CONFIG.single_process_mode
-  if defined?(Resque) && defined?(Redis)
-    before_fork do |server, worker|
+before_fork do |server, worker|
+  unless APP_CONFIG.single_process_mode
+    if defined?(Resque) && defined?(Redis)
       # Clean up Resque workers killed by previous deploys/restarts
       Resque.workers.each { |w| w.unregister_worker }
       @resque_pid ||= spawn('bundle exec rake resque:work QUEUE=*')
 
-      # disconnect Redis if in use
+      # disconnect redis if in use
       Resque.redis.client.disconnect
     end
+  end
+end
 
-    after_fork do |server, worker|
+after_fork do |server, worker|
+  unless APP_CONFIG.single_process_mode
+    if defined?(Resque) && defined?(Redis)
       Resque.redis = Redis.new(url: APP_CONFIG.redis.url)
     end
   end
