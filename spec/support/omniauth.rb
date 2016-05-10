86c47f7e064ead6a7bd28bd7f3881b58313f723d
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-05-10 14:52:22 +0200

    fix rubocop offences

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -8,11 +8,13 @@ OMNIAUTH_MOCKED_AUTHHASH = OmniAuth::AuthHash.new(provider: 'facebook',
                                                     first_name: 'John',
                                                     last_name: 'Doe',
                                                     image: 'http://graph.facebook.com/123456/picture?type=square',
-                                                    urls: { 'Facebook' => 'http://www.facebook.com/profile.php?id=123456' } },
+                                                    urls: { 'Facebook' => 'http://www.facebook.com/profile.php?id=123456' }
+                                                  },
                                                   credentials: {
                                                     token: 'facebook token',
                                                     expires_at: 2.days.from_now.to_i,
-                                                    expires: true },
+                                                    expires: true
+                                                  },
                                                   extra: {
                                                     raw_info: {
                                                       id: '123456',
@@ -25,10 +27,12 @@ OMNIAUTH_MOCKED_AUTHHASH = OmniAuth::AuthHash.new(provider: 'facebook',
                                                       work: [
                                                         { employer: { id: '100', name: 'First Inc.' }, start_date: '0000-00' },
                                                         { employer: { id: '101', name: 'Second Ltd.' }, start_date: '0000-00' },
-                                                        { employer: { id: '102', name: 'Third S.p.A.' }, start_date: '0000-00', end_date: '0000-00' }],
+                                                        { employer: { id: '102', name: 'Third S.p.A.' }, start_date: '0000-00', end_date: '0000-00' }
+                                                      ],
                                                       favorite_athletes: [
                                                         { id: '200', name: 'First Athlete' },
-                                                        { id: '201', name: 'Second Athlete' }],
+                                                        { id: '201', name: 'Second Athlete' }
+                                                      ],
                                                       education: [{ school: { id: '300', name: 'A College' }, type: 'College' }],
                                                       gender: 'male',
                                                       email: 'test@example.com',
@@ -37,8 +41,11 @@ OMNIAUTH_MOCKED_AUTHHASH = OmniAuth::AuthHash.new(provider: 'facebook',
                                                       languages: [
                                                         { id: '113153272032690', name: 'Italian' },
                                                         { id: '106059522759137', name: 'English' },
-                                                        { id: '108224912538348', name: 'French' }],
-                                                      updated_time: '2012-12-16T08:49:27+0000' } })
+                                                        { id: '108224912538348', name: 'French' }
+                                                      ],
+                                                      updated_time: '2012-12-16T08:49:27+0000'
+                                                    }
+                                                  })
 
 OmniAuth.config.test_mode = true
 OmniAuth.config.mock_auth[:facebook] = OMNIAUTH_MOCKED_AUTHHASH
