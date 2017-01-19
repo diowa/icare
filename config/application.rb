7ce0c39e733feb6a90ed663792fe494292576bd2
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-19 18:38:35 +0100

    Update Rails files

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 # Pick the frameworks you want:
 require 'active_model/railtie'
@@ -21,11 +21,6 @@ module Icare
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.enforce_available_locales = false
     config.i18n.default_locale = :'en-US'
