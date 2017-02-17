fb8935f0c7ac0deb9140a15498a3da9d6c5b9eb6
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-02-17 14:42:56 +0100

    Remove useless check
    Base controller inherits from application controller, which requires the
    user to be authenticated by default

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,7 +6,7 @@ module Admin
     private
 
     def check_admin
-      redirect_to root_path, flash: { error: t('flash.errors.not_allowed') } if user_signed_in? && !current_user.admin?
+      redirect_to root_path, flash: { error: t('flash.errors.not_allowed') } unless current_user.admin?
     end
   end
 end
