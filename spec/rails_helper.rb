bb8a9e3eb0ab95e81153758e62e42ffb87c3843a
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-06-10 13:53:27 +0200

    Revert "Move coveralls and simplecov config to spec helper"
    This reverts commit 8cb20bfda19983ab9592347f10a93685f8737d88.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear! 'rails'
+end
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
