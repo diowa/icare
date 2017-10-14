c59944d4166c49c005ac57393c8ceedd34877b5a
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-10-14 14:52:31 +0200

    Opt out from clear site data

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -16,25 +16,7 @@ SecureHeaders::Configuration.default do |config|
   config.x_download_options                = 'noopen'
   config.x_permitted_cross_domain_policies = 'none'
   config.referrer_policy                   = 'origin-when-cross-origin'
-  config.clear_site_data = %w[cache cookies storage executionContexts]
+  config.clear_site_data = SecureHeaders::OPT_OUT
 
   config.csp = SecureHeaders::OPT_OUT
-  # config.csp = {
-  #   default_src: %w(https: 'self'),
-  #   frame_src: %w(*.facebook.com),
-  #   connect_src: %w('self'),
-  #   font_src: %w('self' data: fonts.gstatic.com netdna.bootstrapcdn.com),
-  #   img_src: %w('self' data: *.fbcdn.net *.googleapis.com *.gstatic.com *.facebook.com),
-  #   media_src: %w('none'),
-  #   object_src: %w('none'),
-  #   script_src: %w('self' 'unsafe-eval' 'unsafe-inline' maps.googleapis.com connect.facebook.net),
-  #   style_src: %w('self' 'unsafe-inline' fonts.googleapis.com netdna.bootstrapcdn.com),
-  #   base_uri: %w('self'),
-  #   child_src: %w('self'),
-  #   form_action: %w('self'),
-  #   frame_ancestors: %w('none'),
-  #   plugin_types: %w(),
-  #   block_all_mixed_content: true,
-  #   report_uri: %w(/uri-directive)
-  # }
 end
