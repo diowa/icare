67fc1959de2e6a385eaa1241a7a2951407db3191
Geremia Taglialatela <tagliala.dev@gmail.com>
2020-01-01 22:26:04 +0100

    Fix vertical multi select

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -116,7 +116,7 @@ SimpleForm.setup do |config|
     b.use :html5
     b.optional :readonly
     b.use :label, class: 'form-control-label'
-    b.wrapper tag: 'div', class: 'd-flex flex-row justify-content-between align-items-center' do |ba|
+    b.wrapper tag: 'div', class: 'd-flex flex-row justify-content-left align-items-center mx-n1' do |ba|
       ba.use :input, class: 'form-control mx-1', error_class: 'is-invalid', valid_class: 'is-valid'
     end
     b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
