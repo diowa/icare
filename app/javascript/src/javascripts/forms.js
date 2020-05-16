2dd79b71f6f8aa9e6c010c0fac480ea84f1e193f
Geremia Taglialatela <tagliala.dev@gmail.com>
2020-05-16 12:25:27 +0200

    Fix scrolling to first JavaScript error

--- app/javascript/src/javascripts/forms.js
+++ app/javascript/src/javascripts/forms.js
@@ -2,7 +2,7 @@ require('@client-side-validations/client-side-validations')
 require('@client-side-validations/simple-form/dist/simple-form.bootstrap4')
 
 ClientSideValidations.callbacks.form.fail = ($form, eventData) => {
-  const settings = $form[0].ClientSideValidations
+  const settings = $form[0].ClientSideValidations.settings
 
   $('html, body').animate(
     { scrollTop: $form.find(`.${settings.html_settings.wrapper_error_class}`).offset().top - $('.main-navbar').outerHeight() - 10 }
