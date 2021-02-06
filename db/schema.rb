4bd9e395670fa36ed2e05e9a949e4cb8f39acde2
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-02-06 22:52:57 +0100

    Change default feedback status
    Fix #723

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_193118) do
+ActiveRecord::Schema.define(version: 2021_02_06_214754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_193118) do
     t.string "category", default: "bug"
     t.text "message"
     t.string "url"
-    t.string "status", default: "open"
+    t.string "status", default: "normal"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
