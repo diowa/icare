eb88ac511960519022401669a14e50a8f3742463
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-02-13 16:29:19 +0100

    Refactor assets folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,29 +3,27 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'src/application.scss'
-
 import {} from 'jquery-ujs'
 
 require.context('../images', true)
 
 require('turbolinks').start()
 
-require('src/javascripts/initialize-on-event')
+require('src/initialize-on-event')
 
 require('src/bootstrap')
 require('src/fontawesome')
 
-require('src/javascripts/vendor/i18n-translations')
+require('src/vendor/i18n-translations')
 
-require('src/javascripts/handlebars')
-require('src/javascripts/conversations')
-require('src/javascripts/forms')
-require('src/javascripts/maps')
-require('src/javascripts/maps-new')
-require('src/javascripts/maps-search')
-require('src/javascripts/navbar')
-require('src/javascripts/pages')
+require('src/handlebars')
+require('src/conversations')
+require('src/forms')
+require('src/maps')
+require('src/maps-new')
+require('src/maps-search')
+require('src/navbar')
+require('src/pages')
 
 // Expose jQuery to window
 window.$ = window.jQuery = $
