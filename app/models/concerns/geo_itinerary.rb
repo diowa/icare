fa7bd4c2a72bd3850321fb43f459f41d3958bc92
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-03-02 09:38:56 +0100

    Revert "Revert false positive optimization"
    This reverts commit 354f03449308067253aedc904bb234dbfe5c7a43.

--- app/models/concerns/geo_itinerary.rb
+++ app/models/concerns/geo_itinerary.rb
@@ -25,12 +25,12 @@ module GeoItinerary
     end
 
     def overview_path=(value)
-      value = "LINESTRING(#{value.map { |p| p.join(' ') }.join(', ')})" if value.is_a?(Array) && value.all? { |p| p.is_a?(Array) }
+      value = "LINESTRING(#{value.map { |p| p.join(' ') }.join(', ')})" if value.is_a?(Array) && value.all?(Array)
       super value
     end
 
     def via_waypoints=(value)
-      value = "MULTIPOINT(#{value.map { |p| p.join(' ') }.join(', ')})" if value.is_a?(Array) && value.all? { |p| p.is_a?(Array) }
+      value = "MULTIPOINT(#{value.map { |p| p.join(' ') }.join(', ')})" if value.is_a?(Array) && value.all?(Array)
       super value
     end
 
