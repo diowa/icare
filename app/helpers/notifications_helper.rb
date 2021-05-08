9b775bf8aeb5da7cde2aaf11771fdda5de2fcf85
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-05-08 12:32:46 +0200

    Update to Bootstrap 5

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,7 +3,7 @@
 module NotificationsHelper
   def notifications_tag(type, icon)
     tag.li class: 'notifications', id: "notifications-#{type}" do
-      link_to send(:"#{type}_path"), data: { notifications_type: type, toggle: 'popover', unread: current_user.public_send(:"unread_#{type}_count"), remote: public_send(:"unread_#{type}_path", format: :json) } do
+      link_to send(:"#{type}_path"), data: { notifications_type: type, bs_toggle: 'popover', unread: current_user.public_send(:"unread_#{type}_count"), remote: public_send(:"unread_#{type}_path", format: :json) } do
         tag.span(nil, class: "notifications-icon fa fa-#{icon}") + tag.span(nil, class: 'unread-count')
       end
     end
