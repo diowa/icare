ef0f262de50d929102e4f202a47e6ae8439ac33b
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-09-18 11:24:03 +0200

    Rails 7.0

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require 'action_mailer/railtie'
 # require "action_text/engine"
 require 'action_view/railtie'
 # require "action_cable/engine"
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,7 +23,7 @@ Bundler.require(*Rails.groups)
 module Icare
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
